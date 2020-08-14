810cf0161a9b9fa786ee60fcf5ec4a35db4bc97d
Negin <nuggetnegin@gmail.com>
2020-08-14 16:47:45 -0400

    api notes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,5 @@
+//req is an instance of http.IncomingMessage, plus some pre-built middlewares you can see here.
+//res is an instance of http.ServerResponse, plus some helper functions you can see here.
 export default (req, res) => {
     res.status(200).json({ text: 'Hello' })
-  }
+}
