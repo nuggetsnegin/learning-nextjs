74a1763f69b5ecea0102fd227f7be59ee837df1e
Negin <nuggetnegin@gmail.com>
2020-08-14 11:26:58 -0400

    global styles - done

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 //loading global CSS files: 'App' component is a top level component which will be common across different pages
+//must be imported from pages/_app.js will not work anywhere else
+import '../styles/global.css' //pages get access to global styles
+
 export default function App({ Component, pageProps }){
     return <Component {...pageProps} />
 }
